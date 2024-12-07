51b9c7d88d8dd0dca08b17818c5d6eae26b3ad9a
Vilyam <z.v.m.365@gmail.com>
2024-12-07 18:27:55 +0300

    changed the url and port

--- src/settings.ts
+++ src/settings.ts
@@ -2,9 +2,9 @@ import {config} from "dotenv";
 config();
 
 const SETTINGS = {
-    PORT: process.env.PORT || 3003,
+    PORT: process.env.PORT || 5000,
     PATH: {
-        VIDEOS: '/hometask_01/api/videos',
+        VIDEOS: '/videos',
     },
 };
 
